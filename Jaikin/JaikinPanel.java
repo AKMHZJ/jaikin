9ac15c6c55b6b224e131a4d02c630aab741fd1e1
Marouane-EN <173193132+Marouane-EN@users.noreply.github.com>
2026-03-08 08:50:14 +0000

    implement mouse and key event handlers

--- Jaikin/JaikinPanel.java
+++ Jaikin/JaikinPanel.java
@@ -147,4 +147,28 @@ public class JaikinPanel extends JPanel implements MouseListener, MouseMotionLis
         }
     }
 
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
 }
